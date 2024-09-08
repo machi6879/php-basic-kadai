e1d7da328b1d657de4740c9aef2a37c67de286c1
machi6879 <haruna6399@gmail.com>
2024-09-08 16:04:00 +0900

    PHP課題007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列</title>
+</head>  
+
+  <body>
+    <p>
+        <?php
+        $vegetables = ['name' => 'onion','price' => 200,'weight' => 160,];
+
+        print_r($vegetables);
+
+        ?>
+
+    </P>
+</body>
+
+</html>
