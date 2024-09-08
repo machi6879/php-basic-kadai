8cff265c652e5831b0efba1d1f46295a17f46b6b
machi6879 <haruna6399@gmail.com>
2024-09-08 16:04:49 +0900

    Update associative-array.php

--- kadai_007/associative-array.php
+++ kadai_007/associative-array.php
@@ -9,7 +9,7 @@
   <body>
     <p>
         <?php
-        $vegetables = ['name' => 'onion','price' => 200,'weight' => 160,];
+        $vegetables = ['name' => 'onion','price' => 200,'weight' => 160];
 
         print_r($vegetables);
 
